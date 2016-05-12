492c44e505aaa6188054c89d9a647123365a48c2
Mikkel Malmberg <mikkel@brnbw.com>
2016-05-12 19:56:22 +0200

    Handle is env var

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ require('dotenv').load()
 global.Promise = require('bluebird')
 
 const token = process.env.SLACK_TOKEN
-require('./Client').start(token, 'rd')
+const handle = process.env.HANDLE
+require('./Client').start(token, handle)
